ae7b72dcd06689c5d8c3a96c3fc1b80821bb0b6d
emunte <emunte@idibell.cat>
2023-02-09 17:21:55 +0100

    Build options list

--- runPMS2_vaR.R
+++ runPMS2_vaR.R
@@ -2,13 +2,34 @@
 # USAGE: Rscript runPMS2_vaR.R [-t tools_file] [-d datasets_file]
 
 #libs
-
-
+library(yaml)
+library("optparse")
 # Check runPMS2_vaR.R is being called from PMS2_vaR folder
 
 if (length(list.files(pattern = "runPMS2_vaR.R"))== 0){
   cat("Sorry, runPMS2_vaR.R .R should be called from PMS2_vaR.R  folder\n")
   quit()
 }
+
+
+# Build options list
+ option_list <- list(
+   make_option(c("-t", "--tools"), type="character", default="params/tools.yaml",
+               help="Path to tools file (yaml)", metavar="character"),
+   make_option(c("-b", "--bamTxt"), type="character", default="",
+               help="Path to bams TXT routes. Copy all full paths of your bam files to a txt-file", metavar="character"),
+   make_option(c("-r", "--reference"), type="character", default="",
+               help="Full path to reference file (fa).", metavar="character"),
+   make_option(c("-v", "--vardictjava"), type="character", default="params/vardcitjavaParams.yaml",
+               help="Full path to reference file (fa).", metavar="character")
+ );
+
+# opt_parser <- OptionParser(option_list=option_list);
+
+# Load params
+args <- parse_args(opt_parser);
+tools <- yaml.load_file(args$tools)
+
+
 
 
